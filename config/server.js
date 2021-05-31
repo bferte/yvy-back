d97450912f92469c71848ce73c0ac435cad505f6
Briac ferté <bferte@macbook-pro-de-briac.home>
2021-05-31 19:54:07 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8954658b938dd0a739435c6e1e138e78'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e902bea7-96b2-4886-9012-6e24159ba6bb'
+};
